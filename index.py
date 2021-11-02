547748894c2c32665e6cc9eb05248ae714eb6828
Ivan <ivansentic99@gmail.com>
2021-11-02 18:14:24 +0100

    progprijava

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("Hello world")
+print("Dobrodosli u Unidu sustav")
+print("Za prijavu upišite broj 1, a za registraciju broj 2")
+broj = int(input("Unesite broj:"))
+while broj!=1 and broj!=2:
+    broj = int(input("Unesite broj:"))
+if broj==1:
+    email = input("Unesite email:")
+    lozinka = input("Unesite lozinku:")
+    print("Uspjesna prijava")
+else:
+    email = input("Unesite email:")
+    lozinka = input("Unesite lozinku:")
+    print("Uspjesna registracija")
+    
