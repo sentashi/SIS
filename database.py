c6894576aca150c8b23dfa161b082eb1e1ffdb44
Sentashi <ivansentic99@gmail.com>
2021-11-09 18:25:51 +0100

    bravo

--- database.py
+++ database.py
@@ -5,9 +5,7 @@ cur = con.cursor()
 
 # Create table
 cur.execute('''CREATE TABLE IF NOT EXISTS user
-               (id int primary key, name text, email text, password text, contact text, created_at text)''')
-# Insert a row of data
-cur.execute("INSERT INTO user VALUES ('1', 'Andro','andro@outlook.hr','lozinka123', '+385986473846', '09112021')")
+               (id integer primary key, name text, email text, password text, contact text, created_at text)''')
 
 
 con.commit()
--- index.py
+++ index.py
@@ -1,3 +1,22 @@
+import sqlite3
+from hashlib import blake2b
+con = sqlite3.connect('baza.db')
+
+def register_user(email, lozinka):
+    cur = con.cursor()
+    
+    h = blake2b()
+    transbyte = str.encode(lozinka)
+    h.update(transbyte)
+    #ovo je lozinka
+    
+    vraca = cur.execute("INSERT INTO user (email, password) VALUES (?, ?);", (email, h.hexdigest()))
+    if(vraca==True):
+        print("Uspjesna registracija")
+        print(vraca)
+    con.commit()
+    con.close()
+
 print("Dobrodosli u Unidu sustav")
 print("Za prijavu upišite broj 1, a za registraciju broj 2")
 broj = int(input("Unesite broj:"))
@@ -10,5 +29,5 @@ if broj==1:
 else:
     email = input("Unesite email:")
     lozinka = input("Unesite lozinku:")
-    print("Uspjesna registracija")
+    register_user(email,lozinka)
     
