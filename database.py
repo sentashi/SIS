1a092498978238ee83c0df8c43b2f685ae4c4b2f
Your Name <you@example.com>
2021-11-23 17:25:43 +0100

    commitsa

--- database.py
+++ database.py
@@ -5,8 +5,7 @@ cur = con.cursor()
 
 # Create table
 cur.execute('''CREATE TABLE IF NOT EXISTS user
-               (id integer primary key, name text, email text, password text, contact text, created_at text)''')
-
+               (id integer primary key, name text, email text, password text, contact text, created_at text, login_count integer DEFAULT 0)''')
 
 con.commit()
 con.close()
--- index.py
+++ index.py
@@ -9,14 +9,35 @@ def register_user(email, lozinka):
     transbyte = str.encode(lozinka)
     h.update(transbyte)
     #ovo je lozinka
-    
-    vraca = cur.execute("INSERT INTO user (email, password) VALUES (?, ?);", (email, h.hexdigest()))
-    if(vraca==True):
-        print("Uspjesna registracija")
-        print(vraca)
+    print(transbyte)
+    print(h.hexdigest())
+    cur.execute("INSERT INTO user (email, password) VALUES (?, ?);", (email, h.hexdigest()))
     con.commit()
     con.close()
 
+def login(email, lozinka):
+    cur = con.cursor()
+    
+    h = blake2b()
+    transbyte = str.encode(lozinka)
+    h.update(transbyte)
+    
+    print(email)
+    cur.execute("SELECT * FROM user WHERE email= ?;", (email,))
+    podaci= cur.fetchone()
+    
+      
+
+    if podaci[3]==h.hexdigest():
+        print("Uspjesan login")
+
+        rows = cur.fetchall()
+        brPrijava = podaci[6]
+        curr= con.cursor()
+        curr.execute("UPDATE user SET login_count = ? WHERE id=?;", (brPrijava+1, podaci[0]))
+        con.commit()
+        con.close()
+    
 print("Dobrodosli u Unidu sustav")
 print("Za prijavu upišite broj 1, a za registraciju broj 2")
 broj = int(input("Unesite broj:"))
@@ -25,7 +46,7 @@ while broj!=1 and broj!=2:
 if broj==1:
     email = input("Unesite email:")
     lozinka = input("Unesite lozinku:")
-    print("Uspjesna prijava")
+    login(email, lozinka)
 else:
     email = input("Unesite email:")
     lozinka = input("Unesite lozinku:")
